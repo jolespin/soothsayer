b74fe1fb60eeec068b615c767258a7a1e887b9fc
Josh L. Espinoza <jol.espinoz@gmail.com>
2019-06-17 14:59:22 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ with open("./soothsayer/__init__.py", "r") as f:
         line = line.strip()
         if line.startswith("__version__"):
             version = line.split("=")[-1].strip().strip('"')
+assert version is not None, "Check version in soothsayer/__init__.py"
 
 setup(name='soothsayer',
       version=version,
@@ -15,20 +16,17 @@ setup(name='soothsayer',
       url='https://github.com/jolespin/soothsayer',
       author='Josh L. Espinoza',
       author_email='jespinoz@jcvi.org',
-      license='BSD',
+      license='BSD-3',
       packages=find_packages(include=("*", "./*")),
       install_requires=[
-    # "python >= 3.6",
 	"matplotlib >= 2.2.2",
 	"scipy >= 1.0",
 	"scikit-learn >= 0.20.2",
     "numpy >= 1.13", #, < 1.14.0",
-    # "numpy >= 1.13",
     'pandas >= 0.24.2',
 	'networkx >= 2.0',
 	'ete3 >= 3.0',
 	'scikit-bio >= 0.5.1',
-    # "scikikit-bio >= 0.5.1",
     "biopython >= 1.5",
     "xarray >= 0.10.3",
     "tqdm >=4.19",
@@ -37,6 +35,9 @@ setup(name='soothsayer',
     "rpy2 >= 2.9",
     "matplotlib_venn",
     "palettable >= 3.0.0",
+    "adjustText",
+    "tzlocal",
+
       ],
      include_package_data=True,
      scripts=['bin/clairvoyance.py', "bin/run_soothsayer.py"],
