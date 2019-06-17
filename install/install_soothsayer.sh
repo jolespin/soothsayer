fde0d357c6ddaa61a46fc2861504a75259f3eaa2
Josh L. Espinoza <jol.espinoz@gmail.com>
2019-06-17 15:12:08 -0700

    Add files via upload

--- install/install_soothsayer.sh
+++ install/install_soothsayer.sh
@@ -21,6 +21,7 @@ conda config --add channels bioconda
 conda install -y -c r rpy2 r-devtools
 conda install -y -c bioconda r-dynamictreecut r-wgcna bioconductor-philr bioconductor-edger bioconductor-metagenomeseq bioconductor-phyloseq bioconductor-ggtree ete3 gneiss
 conda install -y -c conda-forge r-ape biopython scikit-bio pandas scikit-learn xarray seaborn numpy networkx scipy matplotlib astropy pyhamcrest tqdm graphviz pydot pygraphviz fastcluster palettable matplotlib-venn python-ternary adjusttext tzlocal
+conda install -y -c jolespin soothsayer
 
 # End
 now=$(date +"%T")
